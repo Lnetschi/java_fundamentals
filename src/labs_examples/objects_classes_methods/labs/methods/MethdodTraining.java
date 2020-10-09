902d80da61193b198d3ca050c10020ad1265b046
Lnetschi <70351200+Lnetschi@users.noreply.github.com>
2020-10-08 22:06:52 -0400

    method labs

--- src/labs_examples/objects_classes_methods/labs/methods/MethdodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethdodTraining.java
@@ -1,2 +1,164 @@
-package labs_examples.objects_classes_methods.labs.methods;public class MethdodTraining {
+package labs_examples.objects_classes_methods.labs.methods;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MethdodTraining {
+    public static void main(String[] args) {
+
+
+        int total = add(2, 3);
+        int total2 = add(4, 5, 6);
+        int bigNum = largestNum(14,12,16,17);
+        int numConsonants = consonants("java is hard");
+        boolean isPrime = prime(29);
+        int[] bigSmall = bigSmall();
+        int[] swap = swapArray();
+        int i = 4;
+        passByValue(i);
+        System.out.println(i);
+        passByReference();
+        divisibleBy(10,2,5);
+
+
+        System.out.println(add(2, 3));
+        System.out.println(add(4, 5, 6));
+        System.out.println(consonants("java is hard"));
+        System.out.println(Arrays.toString(swap));
+        System.out.println(isPrime);
+        System.out.println(bigNum);
+        System.out.println(divisibleBy(10,2,5).size());
+
+        art();
+    }
+
+    //method overloading
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // pass by value
+    public static void passByValue(int j){
+       int i  = j * 10;
+    }
+
+    //pass by reference
+    public static void passByReference(){
+
+        Car c = new Car();
+        c.color = "red";
+        System.out.println(c.color + " before change");
+        updatePassRef(c);
+        System.out.println(c.color + " after change");
+    }
+
+    public static void updatePassRef(Car c){
+        c.color = "blue";
+    }
+
+
+    //largest of 4 numbers
+    private static int largestNum(int a, int b, int c, int d) {
+        int largest = a;
+        if(b > largest)
+            largest = b;
+        if (c > largest)
+            largest = c;
+        if (d > largest)
+            largest = d;
+        return largest;
+    }
+
+    //consonants
+    public static int consonants(String str) {
+
+        int count = 0;
+        String vowels = "aeiou";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(vowels.indexOf(ch) >= 0) {
+                continue;
+            } else if (ch >= 'a' && ch <= 'z'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //art
+    public static void art() {
+
+        System.out.println(" /\\_/\\");
+        System.out.println("( o.o )");
+        System.out.println(" > ^ <");
+    }
+
+    //prime number
+    private static boolean prime(int n) {
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    //array largest and smallest
+    private static int[] bigSmall() {
+
+        int numbers[] = new int [] {4,5,7,2,6};
+        int smallest = numbers[0];
+        int largest = numbers[0];
+
+        for(int i = 1; i < numbers.length; i++) {
+            if(numbers[i] > largest)
+                largest = numbers[i];
+            else if (numbers[i] < smallest)
+                smallest = numbers[i];
+        }
+        System.out.println("largest " + largest);
+        System.out.println("smallest " + smallest);
+        return numbers;
+    }
+
+    //ArrayList
+    public static ArrayList<Integer> divisibleBy(int maxNum, int divisor1, int divisor2){
+
+        ArrayList<Integer> numsArray = new ArrayList<>();
+
+        for(int i = 0; i <= maxNum; i++) {
+            if(i % divisor1 == 0 && i % divisor2 == 0){
+                numsArray.add(i);
+            }
+        }
+        return numsArray;
+    }
+
+    //swap array in place
+    private static int[] swapArray() {
+        int[] nums = {41,25,36};
+        int temp;
+
+        for(int i =0; i < nums.length / 2; i++) {
+            temp = nums[i];
+            nums[i] = nums[nums.length - 1 - i];
+            nums[nums.length -1 - i] = temp;
+        }
+        return nums;
+    }
 }
+
+class Car {
+    String color;
+}
+
+
+
+
+
+
+
--- src/labs_examples/objects_classes_methods/labs/objects/AirplaneLab.java
+++ src/labs_examples/objects_classes_methods/labs/objects/AirplaneLab.java
@@ -8,12 +8,37 @@ public class AirplaneLab {
     public static void main(String[] args) {
 
         Fuel fuel = new Fuel(256.3, "full");
+        fuel.setCurrentFuelLevel("empty");
+        fuel.setFuelCapacity(123.4);
+
         Passenger joe = new Passenger("Joe", "Smith", 36);
+        joe.setFirstName("joe");
+        joe.setLastName("smith");
+        joe.setAge(36);
+
         Passenger bob = new Passenger("Bob", "Smith", 36);
+        bob.setFirstName("bob");
+        bob.setLastName("smith");
+        bob.setAge(36);
+
         Seat seat = new Seat(11, 'a', true);
+        seat.setSeatNumber(11);
+        seat.setSeatLetter('a');
+        seat.setExitRow(true);
+
         Seat chair = new Seat(12, 'b', false);
+        chair.setSeatNumber(12);
+        chair.setSeatLetter('b');
+        chair.setExitRow(false);
+
         Window window = new Window(25,15);
+        window.setHeight(26);
+        window.setWidth(16);
+
         Window glass = new Window(30,20);
+        glass.setHeight(31);
+        glass.setWidth(21);
+
         ArrayList<Passenger> passengers = new ArrayList<>();
         passengers.add(joe);
         passengers.add(bob);
@@ -35,10 +60,26 @@ public class AirplaneLab {
 
 class Fuel {
 
-    double fuelCapacity;
-    String currentFuelLevel;
+    private double fuelCapacity;
+    private String currentFuelLevel;
 
-    public Fuel(double fuelCapacity, String currentFuelLevel) {
+    double getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    String getCurrentFuelLevel() {
+        return currentFuelLevel;
+    }
+
+    void setCurrentFuelLevel(String currentFuelLevel) {
+        this.currentFuelLevel = currentFuelLevel;
+    }
+
+    Fuel(double fuelCapacity, String currentFuelLevel) {
         this.fuelCapacity = fuelCapacity;
         this.currentFuelLevel = currentFuelLevel;
     }
@@ -56,19 +97,40 @@ class Fuel {
 
  class Passenger {
 
-    String firstName;
-    String lastName;
-    int age;
+    private String firstName;
+    private String lastName;
+    private int age;
 
-     public Passenger(String firstName, String lastName, int age) {
+     Passenger(String joe, String smith, int i) {
+     }
+
+     String getFirstName() {
+         return firstName;
+     }
+
+     void setFirstName(String firstName) {
          this.firstName = firstName;
+     }
+
+     String getLastName() {
+         return lastName;
+     }
+
+     void setLastName(String lastName) {
          this.lastName = lastName;
+     }
+
+     int getAge() {
+         return age;
+     }
+
+     void setAge(int age) {
          this.age = age;
      }
 
      @Override
      public String toString() {
-         return "Passengers{" +
+         return "Passenger{" +
                  "firstName='" + firstName + '\'' +
                  ", lastName='" + lastName + '\'' +
                  ", age=" + age +
@@ -78,14 +140,39 @@ class Fuel {
 
 class Seat {
 
-    int seatNumber;
-    char seatLetter;
-    boolean isExitRow;
+    private int seatNumber;
+    private char seatLetter;
+    private boolean isExitRow;
 
-    public Seat(int seatNumber, char seatLetter, boolean isExitRow) {
+    int getSeatNumber() {
+        return seatNumber;
+    }
+
+    void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    char getSeatLetter() {
+        return seatLetter;
+    }
+
+    void setSeatLetter(char seatLetter) {
+        this.seatLetter = seatLetter;
+    }
+
+    boolean isExitRow() {
+        return isExitRow;
+    }
+
+    void setExitRow(boolean exitRow) {
+        isExitRow = exitRow;
+    }
+
+    Seat(int seatNumber, char seatLetter, boolean isExitRow) {
         this.seatNumber = seatNumber;
         this.seatLetter = seatLetter;
         this.isExitRow = isExitRow;
+
     }
 
     @Override
@@ -100,13 +187,33 @@ class Seat {
 
 class Window {
 
-    int height;
-    int width;
+    private int height;
+    private int width;
 
-    public Window(int height, int width) {
+    int getHeight() {
+        return height;
+    }
+
+    void setHeight(int height) {
         this.height = height;
+    }
+
+    int getWidth() {
+        return width;
+    }
+
+    void setWidth(int width) {
         this.width = width;
     }
+
+    Window(int height, int width) {
+        this.height = height;
+        this.width = width;
+
+
+    }
+
+
 
     @Override
     public String toString() {
@@ -118,18 +225,59 @@ class Window {
 }
 
 class Plane {
-    String airlineName;
-    ArrayList<Passenger> passengers;
-    ArrayList<Seat> seats;
-    ArrayList<Window> windows;
-    Fuel fuel;
+    private String airlineName;
+    private ArrayList<Passenger> passengers;
+    private ArrayList<Seat> seats;
+    private ArrayList<Window> windows;
+    private Fuel fuel;
 
-    public Plane(String airlineName, ArrayList<Passenger> passengers, ArrayList<Seat> seats, ArrayList<Window> windows, Fuel fuel) {
+    String getAirlineName() {
+        return airlineName;
+    }
+
+    void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    ArrayList<Seat> getSeats() {
+        return seats;
+    }
+
+    void setSeats(ArrayList<Seat> seats) {
+        this.seats = seats;
+    }
+
+    ArrayList<Window> getWindows() {
+        return windows;
+    }
+
+    void setWindows(ArrayList<Window> windows) {
+        this.windows = windows;
+    }
+
+    Fuel getFuel() {
+        return fuel;
+    }
+
+    void setFuel(Fuel fuel) {
+        this.fuel = fuel;
+    }
+
+    Plane(String airlineName, ArrayList<Passenger> passengers, ArrayList<Seat> seats, ArrayList<Window> windows, Fuel fuel) {
         this.airlineName = airlineName;
         this.passengers = passengers;
         this.seats = seats;
         this.windows = windows;
         this.fuel = fuel;
+
     }
 
     @Override
